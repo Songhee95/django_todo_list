7486d612f0018b7f05daa2f8942791c4f613fe0b
songhee <thdgml5537@gmail.com>
2021-03-21 16:53:59 -0400

    add model and connect to front end

--- write_list/models.py
+++ write_list/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class List(models.Model):
+    todo_list = models.CharField(max_length=5000)
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{}'.format(self.todo_list)
+
+    # class Meta:
+    #     verbose_name_plural = 'Lists'
--- write_list/views.py
+++ write_list/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
 import requests
-
+from . import models
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'index.html')
 
 
 def new_list(request):
     new_list = request.POST.get('todo')
-    print(new_list)
+    exist_list = models.List.objects.all()
+
+    if exist_list.count() == 0:
+        models.List.objects.create(todo_list=new_list)
+
+    try:
+        models.List.objects.get(todo_list=new_list)
+    except:
+        print('no match')
+        models.List.objects.create(todo_list=new_list)
+
+    all_list = models.List.objects.all()
     send_data = {
-        'add_list': new_list
+        'add_list': all_list
     }
     return render(request, 'write_list/new_list.html', send_data)
