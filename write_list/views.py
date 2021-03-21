2cba41bc709d32bd7686cb6c3ae47e97d4115947
songhee <thdgml5537@gmail.com>
2021-03-21 17:59:24 -0400

    add edit and delte button without function

--- write_list/views.py
+++ write_list/views.py
@@ -7,17 +7,17 @@ from . import models
 
 def index(request):
     new_list = request.POST.get('todo')
-    print(new_list)
-    exist_list = models.List.objects.all()
+    if new_list:
+        exist_list = models.List.objects.all()
 
-    if exist_list.count() == 0:
-        models.List.objects.create(todo_list=new_list)
+        if exist_list.count() == 0:
+            models.List.objects.create(todo_list=new_list)
 
-    try:
-        models.List.objects.get(todo_list=new_list)
-    except:
-        print('no match')
-        models.List.objects.create(todo_list=new_list)
+        try:
+            models.List.objects.get(todo_list=new_list)
+        except:
+            print('no match')
+            models.List.objects.create(todo_list=new_list)
 
     all_list = models.List.objects.all()
     send_data = {
