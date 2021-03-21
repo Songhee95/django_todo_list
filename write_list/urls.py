808025e3a5a4c30560264a7a226c2e144252e5f8
songhee <thdgml5537@gmail.com>
2021-03-21 00:38:30 -0400

    form submit and get the data from view file and display

--- write_list/urls.py
+++ write_list/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
+    path('new_list', views.new_list, name='new_list'),
 ]
--- write_list/views.py
+++ write_list/views.py
@@ -5,3 +5,12 @@ import requests
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def new_list(request):
+    new_list = request.POST.get('todo')
+    print(new_list)
+    send_data = {
+        'add_list': new_list
+    }
+    return render(request, 'write_list/new_list.html', send_data)
