5a28e03db38569e94a8871adb05741eb4ef37d6c
songhee <thdgml5537@gmail.com>
2021-03-24 17:08:30 -0400

    add form.py file

--- user_authentication/views.py
+++ user_authentication/views.py
@@ -2,10 +2,22 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+from .models import *
+# from .forms import CreateUserForm
 
 
 def registerPage(request):
     form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            print('-' * 20)
+            print(form)
+            form.save()
+        else:
+            print('not able')
+
     context = {"form": form}
     return render(request, 'user_authentication/register.html', context)
 
