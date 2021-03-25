78479e4945cda890b62e4f283914fcaa4174571e
songhee <thdgml5537@gmail.com>
2021-03-25 17:41:29 -0400

    add checkbox on the list

--- write_list/models.py
+++ write_list/models.py
@@ -6,12 +6,13 @@ from django.contrib.auth.models import User
 class List(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE)
+    cleared = models.BooleanField(default=False)
     todo_list = models.CharField(max_length=5000)
     created = models.DateTimeField(auto_now=True)
     updated_time = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
-        return f" {self.todo_list}, {self.updated_time}, {self.created}"
+        return f"{self.user}, {self.cleared}, {self.todo_list}, {self.updated_time}, {self.created}"
 
     # class Meta:
     #     verbose_name_plural = 'Lists'
