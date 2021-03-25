31c7cbae1f3da410da3e630c58274fb9960fb2bc
songhee <thdgml5537@gmail.com>
2021-03-25 14:56:34 -0400

    delete edit function working with user modesl

--- write_list/views.py
+++ write_list/views.py
@@ -7,10 +7,10 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 
-def getData():
-    all_list = models.List.objects.all()
+def getData(userid):
+    user = models.List.objects.filter(user=userid)
     list_array = []
-    for listEle in all_list:
+    for listEle in user:
         add_list = {
             "li": listEle.todo_list,
             "created": listEle.created,
@@ -26,38 +26,40 @@ def getData():
 
 # @login_required(login_url='login')
 def index(request):
-    username = request.user.username
-    print(username)
+    userid = request.user.id
     new_list = request.POST.get('todo')
     if new_list:
-        exist_list = models.List.objects.all()
-
-        if exist_list.count() == 0:
-            models.List.objects.create(todo_list=new_list).save()
+        exist_list = getData(userid)
+        if exist_list == None:
+            models.List.objects.create(
+                user_id=userid, todo_list=new_list).save()
         try:
-            models.List.objects.get(todo_list=new_list)
+            models.List.objects.get(user_id=userid, todo_list=new_list)
         except:
             print('no match')
-            models.List.objects.create(todo_list=new_list).save()
-    send_data = getData()
+            models.List.objects.create(
+                user_id=userid, todo_list=new_list).save()
+    send_data = getData(userid)
     return render(request, 'write_list/new_list.html', send_data)
 
 
 def delete(request, list_id):
+    userid = request.user.id
     selected = models.List.objects.get(pk=list_id)
     selected.delete()
-    send_data = getData()
+    send_data = getData(userid)
     return redirect('list:index')
 
 
 @ csrf_exempt
 def edit(request, list_id):
+    userid = request.user.id
     edit_string = request.POST.get('string')
     edit_id = list_id
     get_original_value = models.List.objects.get(pk=list_id)
     get_original_value.todo_list = edit_string
     get_original_value.save()
-    send_data = getData()
+    send_data = getData(userid)
     return render(request, 'write_list/new_list.html', send_data)
 
 
