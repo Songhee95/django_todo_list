8a5dafbb20d3ce08e23852cb926db06560efd14f
songhee <thdgml5537@gmail.com>
2021-04-04 00:49:23 -0400

    add modal to home schedule cards

--- static/js/cardBtn.js
+++ static/js/cardBtn.js
@@ -1,5 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".modal");
+  var instances = M.Modal.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function () {
+  $(".modal").modal();
+});
+
 const cardBtnElement = $(".individual_card_wrap");
 cardBtnElement.click(function (e) {
   const selected = e.target.closest(".individual_card_wrap");
-  const selectedData = selected.getAttribute("data-id");
+  const selectedDate = selected.getAttribute("data-id");
+  const modalContainer = $(".modal_content__header");
+  modalContainer.text(selectedDate);
 });
