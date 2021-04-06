17ec16c4d2ed69e2c7e09255e924fd120d8f4bec
songhee <thdgml5537@gmail.com>
2021-04-05 22:57:58 -0400

    display todo list by filtering card

--- static/js/cardBtn.js
+++ static/js/cardBtn.js
@@ -10,20 +10,25 @@ $(document).ready(function () {
 });
 
 const cardBtnElement = document.querySelectorAll(".individual_card_wrap");
+
 for (el of cardBtnElement) {
   el.addEventListener("click", function (e) {
+    const list_container = document.getElementById(
+      "modal_content_list_contents"
+    );
+    list_container.innerHTML = "";
     const selected = e.target.closest(".individual_card_wrap");
     const selectedDate = selected.getAttribute("data-id");
     const modalContainer = $(".modal_content__header");
+
     modalContainer.text(selectedDate);
 
     const getting_date_list = document.getElementsByClassName(
       "home_list_set_wrap"
     );
-    const list_container = document.getElementById(
-      "modal_content_list_contents"
-    );
+
     let adding_list_container = document.createElement("div");
+    adding_list_container.setAttribute("class", "added_modal_content_div");
     for (var list of getting_date_list) {
       const get_data_content = list.getAttribute("data-content");
       if (get_data_content == selectedDate) {
@@ -31,12 +36,7 @@ for (el of cardBtnElement) {
         adding_list_container.append(cln);
       }
     }
-
-    console.log(adding_list_container);
-    list_container.parentNode.replaceChild(
-      adding_list_container,
-      list_container
-    );
+    list_container.append(adding_list_container);
     // ajax call
     // $.ajax({
     //   type: "POST",
