f359f3b539942e422c5f43070453d3ec9c3e298e
songhee <thdgml5537@gmail.com>
2021-04-21 11:47:13 -0400

    login/ register page styling change

--- user_authentication/forms.py
+++ user_authentication/forms.py
@@ -4,6 +4,15 @@ from django.contrib.auth.models import User
 
 
 class ShRegisterForm(UserCreationForm):
+    username = forms.CharField(label='username', widget=forms.TextInput(
+        attrs={'placeholder': 'Username'}))
+    email = forms.CharField(label='email', widget=forms.TextInput(
+        attrs={'placeholder': 'Email address'}))
+    password1 = forms.CharField(label='password', widget=forms.TextInput(
+        attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput(
+        attrs={'placeholder': 'Confirm password'}))
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
