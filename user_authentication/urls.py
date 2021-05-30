e26728a6571b2983d409bcd03753ef60f787bbe7
songhee <thdgml5537@gmail.com>
2021-05-30 18:58:30 -0400

    forgot password pages styling

--- user_authentication/urls.py
+++ user_authentication/urls.py
@@ -11,13 +11,5 @@ urlpatterns = [
     path('forgotPassword/', auth_views.PasswordResetView.as_view(template_name='user_authentication/forgotPassword.html'),
          name='password_reset'),
 
-    path('pwd_reset_sent/', auth_views.PasswordResetDoneView.as_view(template_name='user_authentication/pwd_reset_sent.html'),
-         name="password_reset_done"),
-
-    path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name='user_authentication/pwd_reset_confirm.html'), name='password_reset_confirm'),
-
-    path('pwd_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='user_authentication/pwd_reset_complete.html'),
-         name="password_reset_complete"),
 
 ]
